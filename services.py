9f702bf438dc39c7cc78dd7cd9c6cc2111263e68
satwik mishra <satwikmishra074@gmail.com>
2026-03-06 01:09:13 +0530

    added port->service mapping file

---
+++ services.py
@@ -0,0 +1,18 @@
+# Basic port → service mapping.
+services = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    443: "HTTPS",
+    3306: "MySQL",
+    8080: "HTTP-Proxy"
+}
+
+def get_service(port):
+
+    return services.get(port, "Unknown")
